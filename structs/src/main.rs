f1df90cc46c1786a1ab1d588b485c7f68b458682
Karol Moroz <karol@moroz.dev>
2022-09-10 19:31:23 +0200

    Parse question ids using trait

--- structs/src/main.rs
+++ structs/src/main.rs
@@ -1,3 +1,7 @@
+use std::io::Error;
+use std::io::ErrorKind;
+use std::str::FromStr;
+
 #[derive(Debug)]
 struct Question {
     id: QuestionId,
@@ -8,6 +12,17 @@ struct Question {
 
 #[derive(Debug)]
 struct QuestionId(String);
+
+impl FromStr for QuestionId {
+    type Err = std::io::Error;
+
+    fn from_str(id: &str) -> Result<Self, Self::Err> {
+        match id.is_empty() {
+            false => Ok(QuestionId(id.to_string())),
+            true => Err(Error::new(ErrorKind::InvalidInput, "No id provided")),
+        }
+    }
+}
 
 impl Question {
     fn new(id: QuestionId, title: String, content: String, tags: Option<Vec<String>>) -> Self {
@@ -22,7 +37,7 @@ impl Question {
 
 fn main() {
     let question = Question::new(
-        QuestionId("1".to_string()),
+        QuestionId::from_str("1").unwrap(),
         "Hello world".to_string(),
         "Some content".to_string(),
         None,
