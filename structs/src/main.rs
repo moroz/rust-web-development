772a116730185310da82bd6aaebf21570f37e560
Karol Moroz <karol@moroz.dev>
2022-09-10 19:28:39 +0200

    Implement instantiating structs

--- structs/src/main.rs
+++ structs/src/main.rs
@@ -1,3 +1,31 @@
+#[derive(Debug)]
+struct Question {
+    id: QuestionId,
+    title: String,
+    content: String,
+    tags: Option<Vec<String>>,
+}
+
+#[derive(Debug)]
+struct QuestionId(String);
+
+impl Question {
+    fn new(id: QuestionId, title: String, content: String, tags: Option<Vec<String>>) -> Self {
+        Self {
+            id,
+            title,
+            content,
+            tags,
+        }
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let question = Question::new(
+        QuestionId("1".to_string()),
+        "Hello world".to_string(),
+        "Some content".to_string(),
+        None,
+    );
+    println!("{:?}", question);
 }
